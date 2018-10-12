a0fb674fc0c2496d0a8d3d31f125e1942d27a002
Ubuntu <342058040@qq.com>
2018-10-12 15:51:57 +0000

    format kmeans code

--- nt_spark/utils.py
+++ nt_spark/utils.py
@@ -8,30 +8,40 @@ from nt_spark.spark_sql_base import SparkSql
 from pyspark.mllib.clustering import (
     KMeans, KMeansModel
 )
+from pyspark.mllib.tree import (
+    RandomForest, RandomForestModel
+)
 from numpy import array
 from math import sqrt
 
 
 class SparkAnomaly(object):
-    def __init__(self):
+    def __init__(self, appid, start_time, end_time):
+        self.appid = appid
+        self.start_time = start_time
+        self.end_time = end_time
         self.spark_sql = SparkSql()
         self.cat_res = self.spark_sql.load_table_dataframe('cat_resource')
+        self.filter_str = "appid = {0} " \
+                          "and create_time >= {1} " \
+                          "and update_time <= {2}".format(
+            self.appid, self.start_time, self.end_time,
+        )
 
-    def get_kmeans_model(self, appid=None):
+    def get_kmeans_model(self):
         """
         读取表的dataframe
-        :param appid:
         :return:
         """
-        filter_str = "appid = {0}".format(
-            appid
-        )
-        df = self.cat_res.filter(filter_str)
+        df = self.cat_res.filter(self.filter_str)
         parsed_data_rdd = df.rdd.map(lambda x: array([x[4], x[5], x[6]]))
-        print(parsed_data_rdd.collect())
 
         # 建立聚类模型
-        clusters = KMeans.train(parsed_data_rdd, 2, maxIterations=10, initializationMode="random")
+        clusters = KMeans.train(
+            parsed_data_rdd, 2,
+            maxIterations=10,
+            initializationMode="random"
+        )
 
         # Evaluate clustering by computing Within Set Sum of Squared Errors
         def error(point):
@@ -51,24 +61,15 @@ class SparkAnomaly(object):
         # # )
         # # return trained_model
 
-    def get_random_forest_model(self, appid=None):
+    def get_random_forest_model(self):
         """
         读取表的dataframe
-        :param appid:
         :return:
         """
-        filter_str = "appid = {0}".format(
-            appid
-        )
-        df = self.cat_res.filter(filter_str)
+        df = self.cat_res.filter(self.filter_str)
 
-        # 统计排序
-        res_df = df.groupBy(
-            "cognition_map_num"
-        ).count().sort('count', ascending=False)
-
-        return res_df
+        pass
 
 
-test = SparkAnomaly()
-test.get_kmeans_model(110312)
+test = SparkAnomaly(110312, 1539331733000, 1539334343000)
+test.get_kmeans_model()
