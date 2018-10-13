3b75f60152ced44b926245a3b89a10d604757baf
Ubuntu <342058040@qq.com>
2018-10-13 10:00:17 +0000

    update requirements

--- nt_spark/models.py
+++ nt_spark/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+from nt_core.models import BaseModel
+
+
+class CatResultData(BaseModel):
+    appid = models.IntegerField(blank=True, null=True)
+
+
+    class Meta:
+        db_table = 'cat_normal_resource'
